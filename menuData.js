b3d6ccc62cd167abc0b90b018483b01815347999
Gobato15 <gobato59@gmail.com>
2026-04-05 16:05:11 -0300

    Cardapio16

--- menuData.js
+++ menuData.js
@@ -134,7 +134,7 @@ window.menuItemsData = [
         category: "desserts",
         price: 8.00,
         description: "Bolo branco com camadas de mousse de maracujá fresco e delicioso.",
-        image: "assets/bolo_pote_maracuja.png"
+        image: "assets/bolo_maracuja_novo.png"
     },
     {
         id: "20",
